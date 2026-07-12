d0ba7b9bf6ea485e4cd0ee3ffd212a04659755ec
Dominic0074 <61750782+Dominic0074@users.noreply.github.com>
2026-07-12 22:10:07 +0200

    trt

--- Mass Recruting/dist/Mass Recruting.user.js
+++ Mass Recruting/dist/Mass Recruting.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Mass Recruting
 // @namespace    https://github.com/Dominic0074/DieSt-mme
-// @version      0.1.19
+// @version      0.1.20
 // @description  Mass Recruting fuer Die Staemme mit Safety und Status-Banner.
 // @author       kk
 // @match        https://*.die-staemme.de/game.php*
